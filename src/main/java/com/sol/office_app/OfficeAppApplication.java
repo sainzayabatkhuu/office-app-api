1cdeb692d9c67f6941039cd360130231320291e3
Sainzaya Batkhuu <sainzaya.b@gmail.com>
2025-06-02 15:49:23 -0500

    init project files

---
+++ src/main/java/com/sol/office_app/OfficeAppApplication.java
@@ -0,0 +1,13 @@
+package com.sol.office_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeAppApplication.class, args);
+	}
+
+}
