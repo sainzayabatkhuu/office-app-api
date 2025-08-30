ba8a89e8ef5774ab48d2154deeac4fa7d3818840
Sainzaya Batkhuu <sainzaya.b@gmail.com>
2025-08-29 23:53:03 -0500

    inserting core params

--- src/main/java/com/sol/office_app/repository/CoreParameterRepository.java
+++ src/main/java/com/sol/office_app/repository/CoreParameterRepository.java
@@ -1,15 +1,12 @@
 package com.sol.office_app.repository;
 
 import com.sol.office_app.entity.CoreParameter;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface CoreParameterRepository extends JpaRepository<CoreParameter, Long>, JpaSpecificationExecutor<CoreParameter> {
-    boolean existsByParamCode(String paramCode);
-
-    Page<CoreParameter> findAllByParamType(String paramType, Pageable pageable);
+@Repository
+public interface CoreParameterRepository extends
+        JpaRepository<CoreParameter, Long>,
+        JpaSpecificationExecutor<CoreParameter> {
 }
